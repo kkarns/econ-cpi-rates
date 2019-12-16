34f206f96879b7d7d8a94426a98edbcf0720efb1
Kevin <kkarns@gmail.com>
2019-12-16 11:10:44 -0700

    added logging

--- econ-cpi-rates.py
+++ econ-cpi-rates.py
@@ -21,6 +21,7 @@
 ## updated:
 ##      -- Tuesday, May 14, 2019 1:01 PM - initial work with API from https://github.com/OliverSherouse/bls/
 ##      -- Friday, December 6, 2019 4:58 PM - integrated keyring library instead of securestring
+##      -- Monday, December 16, 2019 9:05 AM - added logging
 ##
 ## todo:
 
@@ -32,9 +33,28 @@ import pandas
 import sys
 import pyodbc
 import datetime
+import time
 import timestring
 import keyring
 import getpass
+import logging 
+                     
+## set local code path
+myDir = os.path.dirname(__file__)
+
+## setup the logfile
+logDir = myDir + '/logs' 
+if not os.path.exists(logDir):
+   os.makedirs(logDir)
+logFile = logDir + '/econ-cpi-rates-' + str(time.strftime('%Y-%m-%d-%H%M%S')) + '.log'
+logHandler = logging.FileHandler(os.environ.get("LOGFILE", logFile))
+logHandler.setFormatter(logging.Formatter('%(levelname)s:%(asctime)s:  %(message)s'))
+infoLogger = logging.getLogger()
+infoLogger.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+infoLogger.addHandler(logHandler)
+
+print ('results are logged to ' + logFile)
+infoLogger.info('results are logged to ' + logFile)
 
 ## using the current time - default to previous month
 now = datetime.datetime.now()
@@ -42,29 +62,29 @@ lastMonth = now.month - 1 if now.month > 1 else 12
 lastMonthYear = now.year if now.month > 1 else now.year - 1 
 blsYear = lastMonthYear
 blsMonth = lastMonth
-## print('Default is last month:  ' + str(blsMonth) + '-' + str(blsYear))  ## debug
 
 ## check for 2 arguments: Year and Month and override if available
-## print('This is the name of the script:  ', sys.argv[0])  ## debug
-## print('Number of arguments:  ', len(sys.argv)) ## debug
-## print('The arguments are:  ' , str(sys.argv)) ## debug
 if len(sys.argv) > 2:
-   ## print('Overriding default with these values:  ') ## debug
-   ## print('Year:  ', sys.argv[1]) ## debug
-   ## print('Month: ', sys.argv[2]) ## debug
    blsYear = sys.argv[1]
    blsMonth = sys.argv[2]
-   ## print('Comparing dates... ' + str(blsMonth) + '/1/' + str(blsYear) + ' should be older than ' + str(lastMonth) + '/1/' + str(lastMonthYear) )  ## debug
 
    if timestring.Date(str(blsMonth) + '/1/' + str(blsYear)) > timestring.Date(str(lastMonth) + '/1/' + str(lastMonthYear)):
-      print('Error.  Date cannot be in future.  Please set the override date to less than the current month')
+      msg = 'Error.  Date cannot be in future.  Please set the override date to less than the current month'
+      print (msg)
+      infoLogger.info(msg)
       sys.exit(1)  
-   print('Using override date, which is:  ' + str(blsMonth) + '/1/' + str(blsYear) )      
+   msg = 'Using override date, which is:  ' + str(blsMonth) + '/1/' + str(blsYear) 
+   print (msg)
+   infoLogger.info(msg)
 else:          
-   print('No arguments provided, so using default date, which is the previous month:  ' + str(blsMonth) + '/1/' + str(blsYear) )      
+   msg = 'No arguments provided, so using default date, which is the previous month:  ' + str(blsMonth) + '/1/' + str(blsYear) 
+   print (msg)
+   infoLogger.info(msg)
 
 ## check acount running script while troubleshooting ADS issues
-print (getpass.getuser())
+msg = 'using this account:  ' + getpass.getuser()
+print (msg)
+infoLogger.info(msg)
 
 ## retrieve our Bureau of Labor Statistics Public Data API key from OS environment value  
 ## key comes from https://data.bls.gov/registrationEngine/
@@ -73,26 +93,35 @@ print (getpass.getuser())
 try:  
    os.environ["BLS_API_KEY"]
 except KeyError: 
-   print("Please set the environment variable BLS_API_KEY with our key from the DOL")
+   msg = "Please set the environment variable BLS_API_KEY with our key from the DOL"
+   print(msg)
+   infoLogger.info(msg)
    sys.exit(1)
 BLS_API_KEY  = os.environ.get('BLS_API_KEY')
-## print('The BLS_API_KEY for us, as stored in the registry is:  ' + BLS_API_KEY)    ## debug
 
 
 ## call the API from the BLS python package for the desired year - a year of data values
 cpi_information = bls.get_series('CUUR0000SA0', blsYear, blsYear, BLS_API_KEY)  
 cpi_information.head()
 inf=pandas.DataFrame(cpi_information)
-## print(inf)    ## debug - to see the full output from the API
-## print('Note:  data above is the whole year, data below is the single month.')  ## debug
+msg = 'The raw data from the DOL API for the year parameters is:'
+print (msg)
+infoLogger.info(msg)
+print (inf)
+infoLogger.info(inf)
+
 
 ## filter only the CPI for the desired month 
 try:
    var = inf.loc[str(blsYear)+'-'+str(blsMonth),:].item()
 except KeyError:
-   print('The month requested ' + str(blsMonth) + '-' + str(blsYear) + ' is not available yet, it may take a few more days, often after the 10th day.')
+   msg = 'The month requested ' + str(blsMonth) + '-' + str(blsYear) + ' is not available yet, it may take a few more days, often after the 10th day.'
+   print(msg)
+   infoLogger.info(msg)
    sys.exit(1)
-## print(var) ## debug
+msg = 'The value for the month requested ' + str(blsMonth) + '-' + str(blsYear) + ' is: ' + str(var)
+print (msg)
+infoLogger.info(msg)
 
 ## get credentials from Windows.Security.Credentials.PasswordVault, gui editor: rundll32 keymgr.dll, KRShowKeyMgr
 service_id = 'econ-cpi-rates'
@@ -106,24 +135,35 @@ conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';
 cursor = conn.cursor()
 ## SELECT CPI_YR, CPI_MTH, CPI_RATE FROM BaseData.gis.ECON_CPI_RATES WHERE CPI_YR = 2019 AND CPI_MTH = 1 ORDER BY CPI_YR DESC, CPI_MTH DESC;
 tsql = "SELECT CPI_YR, CPI_MTH, CPI_RATE FROM BaseData.gis.ECON_CPI_RATES WHERE CPI_YR = " + str(blsYear) + " AND " + "CPI_MTH = " + str(blsMonth) + " ORDER BY CPI_YR DESC, CPI_MTH DESC;"
-## print(tsql) ## debug
+infoLogger.info(tsql)
 
 with cursor.execute(tsql):
-    row = cursor.fetchall()
-    print (str(len(row)) + ' existing match(es)')
-    if len(row) > 0 :
-      print ('This CPI value is already in the database:  CPI_YR = ' + str(blsYear) + ' AND ' + 'CPI_MTH = ' + str(blsMonth))
-    else:    
+   row = cursor.fetchall()
+   msg = str(len(row)) + ' existing match(es)'
+   print (msg)
+   infoLogger.info(msg)
+   if len(row) > 0 :
+      msg = 'This CPI value is already in the database:  CPI_YR = ' + str(blsYear) + ' AND ' + 'CPI_MTH = ' + str(blsMonth) 
+      print (msg)
+      infoLogger.info(msg)
+   else:    
       tsql = "INSERT INTO BaseData.gis.ECON_CPI_RATES (CPI_YR,CPI_MTH,CPI_RATE) VALUES (?,?,?);"
       with cursor.execute(tsql,blsYear,blsMonth,var):         
          conn.commit()
-         print ('Successfully Inserted!')
+         msg = 'Successfully Inserted! ' + tsql
+         print (msg)
+         infoLogger.info(msg)
 
-print ('The latest 12 consumer price index entries in the database are:')
+msg = 'The latest 12 consumer price index entries in the database are:'
+print (msg)
+infoLogger.info(msg)
 tsql = "SELECT TOP(12) CPI_YR, CPI_MTH, CPI_RATE FROM BaseData.gis.ECON_CPI_RATES ORDER BY CPI_YR DESC, CPI_MTH DESC;"
 with cursor.execute(tsql):
-    row = cursor.fetchone()
-    while row:
-        print (str(row[0]) + " " + str(row[1]) + " " + str(row[2]))
-        row = cursor.fetchone()
+   row = cursor.fetchone()
+   while row:
+      msg = str(row[0]) + " " + str(row[1]) + " " + str(row[2])
+      print (msg)
+      infoLogger.info(msg)
+      row = cursor.fetchone()
 
+sys.exit(1) ## this is the end.  Who knew?
